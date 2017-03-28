53064ec50388e427ca5aacefeb1e9f2de2f24e08
Lee hyunjoo <siwabada@gmail.com>
2017-03-28 23:51:54 +0900

    동영상 분기처리

--- study/views.py
+++ study/views.py
@@ -21,34 +21,40 @@ def post_new(request):
         if form.is_valid():
             post = form.save(commit=False)
             post.author = request.user
-            regx = r'\w{11}'
-            result = re.search(regx, post.video_url)
-            post.video_key = result.group(0)
-
-            regx_time =  r't=\d{0,2}m?\d{0,2}s?'
-            result_time = re.search(regx_time, post.video_url)
-            result_time = result_time.group(0)
-            result_time = re.findall(r'\d+', result_time)
-            if len(result_time) > 1:
-                post.video_time =  int(result_time[0])*60+int(result_time[1])
-            else:
-                post.video_time = int(result_time[0])
-            post.save()
-
-
-            regx = r'\w{11}'
-            result = re.search(regx, post.video_url2)
-            post.video_key2 = result.group(0)
-
-            regx_time =  r't=\d{0,2}m?\d{0,2}s?'
-            result_time = re.search(regx_time, post.video_url2)
-            result_time = result_time.group(0)
-            result_time = re.findall(r'\d+', result_time)
-            if len(result_time) > 1:
-                post.video_time2 =  int(result_time[0])*60+int(result_time[1])
-            else:
-                post.video_time2 = int(result_time[0])
-            post.save()
+            if len(post.video_url) == 0 :
+                post.save()
+            if len(post.video_url2) == 0 :
+                post.save()
+            if len(post.video_url) > 40 :
+                post.video_key1 = post.video_url1[-11:]
+                post.save()
+            if len(post.video_url2) > 40 :
+                post.video_key2 = post.video_url2[-11:]
+                post.save()
+            if 0 < len(post.video_url) < 40 :
+                # post.video_url 저장
+                post.video_key = post.video_url[17:17+11]
+                regx_time =  r't=\d{0,2}m?\d{0,2}s?'
+                result_time = re.search(regx_time, post.video_url)
+                result_time = result_time.group(0)
+                result_time = re.findall(r'\d+', result_time)
+                if len(result_time) > 1:
+                    post.video_time =  int(result_time[0])*60+int(result_time[1])
+                else:
+                    post.video_time = int(result_time[0])
+                post.save()
+            if 0 < len(post.video_url2) < 40 :
+                # post.video_url2 저장
+                post.video_key2 = post.video_url2[17:17+11]
+                regx_time =  r't=\d{0,2}m?\d{0,2}s?'
+                result_time = re.search(regx_time, post.video_url2)
+                result_time = result_time.group(0)
+                result_time = re.findall(r'\d+', result_time)
+                if len(result_time) > 1:
+                    post.video_time2 =  int(result_time[0])*60+int(result_time[1])
+                else:
+                    post.video_time2 = int(result_time[0])
+                post.save()
             return redirect('study:post_detail', pk=post.pk)
     else:
         form = PostForm()
@@ -63,32 +69,42 @@ def post_edit(request, pk):
         if form.is_valid():
             post = form.save(commit=False)
             post.author = request.user
-            regx = r'\w{11}'
-            result = re.search(regx, post.video_url)
-            post.video_key = result.group(0)
-
-            regx_time =  r't=\d{0,2}m?\d{0,2}s?'
-            result_time = re.search(regx_time, post.video_url)
-            result_time = result_time.group(0)
-            result_time = re.findall(r'\d+', result_time)
-            if len(result_time) > 1:
-                post.video_time =  int(result_time[0])*60+int(result_time[1])
-            else:
-                post.video_time = int(result_time[0])
-
-            regx = r'\w{11}'
-            result = re.search(regx, post.video_url2)
-            post.video_key2 = result.group(0)
-
-            regx_time =  r't=\d{0,2}m?\d{0,2}s?'
-            result_time = re.search(regx_time, post.video_url2)
-            result_time = result_time.group(0)
-            result_time = re.findall(r'\d+', result_time)
-            if len(result_time) > 1:
-                post.video_time2 =  int(result_time[0])*60+int(result_time[1])
-            else:
-                post.video_time2 = int(result_time[0])
-            post.save()
+            if len(post.video_url) == 0 :
+                post.video_key = None
+                post.save()
+            if len(post.video_url2) == 0 :
+                post.video_key2 = None
+                post.save()
+            if len(post.video_url) > 40 :
+                post.video_key1 = post.video_url1[-11:]
+                post.save()
+            if len(post.video_url2) > 40 :
+                post.video_key2 = post.video_url2[-11:]
+                post.save()
+            if 0 < len(post.video_url) < 40 :
+                # post.video_url 저장
+                post.video_key = post.video_url[17:17+11]
+                regx_time =  r't=\d{0,2}m?\d{0,2}s?'
+                result_time = re.search(regx_time, post.video_url)
+                result_time = result_time.group(0)
+                result_time = re.findall(r'\d+', result_time)
+                if len(result_time) > 1:
+                    post.video_time =  int(result_time[0])*60+int(result_time[1])
+                else:
+                    post.video_time = int(result_time[0])
+                post.save()
+            if 0 < len(post.video_url2) < 40 :
+                # post.video_url2 저장
+                post.video_key2 = post.video_url2[17:17+11]
+                regx_time =  r't=\d{0,2}m?\d{0,2}s?'
+                result_time = re.search(regx_time, post.video_url2)
+                result_time = result_time.group(0)
+                result_time = re.findall(r'\d+', result_time)
+                if len(result_time) > 1:
+                    post.video_time2 =  int(result_time[0])*60+int(result_time[1])
+                else:
+                    post.video_time2 = int(result_time[0])
+                post.save()
             return redirect('study:post_detail', pk=post.pk)
     else:
         if post.author == request.user:
