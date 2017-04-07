bd5d508930d0a6cf993200b78c218279c845542a
siwabada <siwabada@gmail.com>
2017-04-08 01:42:37 +0900

    회원가입폼 각 필드 커스터마이징

--- study/forms.py
+++ study/forms.py
@@ -30,8 +30,31 @@ class CommentForm(forms.ModelForm):
 
 
 class CreateUserForm(UserCreationForm):
-    email = forms.EmailField(required=True, help_text='Email 주소를 입력해주세요.')
+    email = forms.EmailField(
+        required=True,
+        label="이메일",
+        widget = forms.TextInput(attrs={'class': 'form-control', 'placeholder': '이메일 주소를 입력해주세요'}),
+        )
+    username = forms.CharField(
+        required=True,
+        label="ID",
+        widget = forms.TextInput(attrs={'class': 'form-control', 'help_text': ''}),
+        )
 
+    password1 = forms.CharField(
+        required=True,
+        label="비밀번호",
+        widget = forms.PasswordInput(attrs={'class': 'form-control', 'placeholder': '숫자와 문자를 포함해 8자리 이상을 입력해주세요'}),
+        )
+
+    password2 = forms.CharField(
+        required=True,
+        label="비밀번호 확인",
+        widget = forms.PasswordInput(attrs={'class': 'form-control', 'placeholder': '같은 비밀번호를 다시 입력해주세요.'}),
+        )
     class Meta:
         model = User
         fields = ('username', 'email', 'password1', 'password2')
+        widgets = {
+            'username' : forms.TextInput(attrs={'class': 'form-control', 'help_text': ''}),
+        }
