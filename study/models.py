ae5d786dc69c57c6943cebd4fffc395523520c9d
siwabada <siwabada@gmail.com>
2017-04-16 23:33:15 +0900

    커버 이미지 적용, 이미지 스타일 수정

--- study/models.py
+++ study/models.py
@@ -21,7 +21,8 @@ class Post(models.Model):
     video_url2 = models.CharField(max_length=100, blank=True, null=True, verbose_name='YouTube 링크2')
     video_key2 = models.CharField(max_length=12, null=True, blank=True)
     video_time2 = models.IntegerField(null=True, blank=True)
-    image = models.ImageField(upload_to = user_path, blank=True)
+    image = models.ImageField(upload_to = user_path, blank=True, verbose_name='이미지 추가', help_text='이미지를 추가할 수 있습니다. not required')
+    cover_img = models.ImageField(upload_to = user_path, blank=True, verbose_name='커버 이미지', help_text='상단의 커버 이미지를 설정할 수 있습니다. not required')
     text = models.TextField(verbose_name='내용')
     created_date = models.DateTimeField(auto_now_add=True)
 
